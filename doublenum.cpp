02e401107304b90923071078cf407ea641a0a314
ruoo21 <143183938+Ruoo21@users.noreply.github.com>
2023-09-17 13:32:44 +0100

    Add semicolon and change std::cin operator

--- doublenum.cpp
+++ doublenum.cpp
@@ -1,12 +1,12 @@
 #include <iostream> 
 
 int main() {
-    std::cout << "Please enter a number: "
+    std::cout << "Please enter a number: ";
     
     int number; // Number to be doubled 
     
-    std::cin << number;
+    std::cin >> number;
 
     std::cout << "That number doubled is: " << number * 2 << "." << '\n';
-    
+
 }
